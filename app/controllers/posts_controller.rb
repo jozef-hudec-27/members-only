f3ac79b79cdd67c06a86091ed06f89753c6a8a64
Jozef Hudec <jojohudec27@gmail.com>
2022-10-07 17:45:16 +0200

    Allow users to create and see posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(title: params[:post][:title], body: params[:post][:body], user: User.first)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
