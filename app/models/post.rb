0a796ead8a0b2c0d4344e2736571ef0db21edf2d
Jozef Hudec <jojohudec27@gmail.com>
2022-10-08 12:09:57 +0200

    Style homepage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { in: 2..120 }
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 250 }
 
   belongs_to :user
   has_many :comments
