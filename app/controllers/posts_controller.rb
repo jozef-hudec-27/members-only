450902f77d07dc8ff395c6c47b93ca85c9bd6952
Jozef Hudec <jojohudec27@gmail.com>
2022-10-09 14:45:34 +0200

    Add minor details

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create edit update]
 
   def index
-    @posts = Post.all
+    @posts = Post.order('updated_at DESC')
   end
 
   def new
