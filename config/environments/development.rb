aba33669d3c458d449cbd58c4e2cddfcc83609a4
Jozef Hudec <jojohudec27@gmail.com>
2022-10-15 11:24:16 +0200

    Add host names

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,5 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << 'members-only.up.railway.app'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,5 @@ Rails.application.configure do
 
 
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << 'members-only.up.railway.app'
 end
